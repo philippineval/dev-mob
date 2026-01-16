327ec1016087ca05cd7bf340722589ea8465d2e3
Philippineval <philippine.vallette@edu.ece.fr>
2026-01-16 13:32:54 +0100

    création du fichier

---
+++ dart_exo1_cours1/bin/dart_exo1_cours1.dart
@@ -0,0 +1,5 @@
+import 'package:dart_exo1_cours1/dart_exo1_cours1.dart' as dart_exo1_cours1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_exo1_cours1.calculate()}!');
+}
---
+++ dart_exo1_cours1/test/dart_exo1_cours1_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_exo1_cours1/dart_exo1_cours1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
