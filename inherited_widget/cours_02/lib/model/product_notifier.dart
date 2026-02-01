4ea3f55b962dbada182a04826ddca9a5df62131f
Philippineval <philippine.vallette@edu.ece.fr>
2026-02-01 20:38:58 +0100

    Ajout d’un barcode fictif lors du chargement du produit

--- inherited_widget/cours_02/lib/model/product_notifier.dart
+++ inherited_widget/cours_02/lib/model/product_notifier.dart
@@ -4,10 +4,22 @@ import 'product.dart';
 class ProductNotifier extends ChangeNotifier {
   Product? _product;
 
+  ProductNotifier() {
+    loadProduct();
+  }
+
   Product? get product => _product;
 
   void loadProduct() {
-    _product = generateProduct();
+    _product = Product(
+  barcode: '123456789',
+  name: 'Produit test',
+  brands: ['Marque test'],
+  nutriScore: ProductNutriScore.A,
+  novaScore: ProductNovaScore.group1,
+  greenScore: ProductGreenScore.A,
+);
+
     notifyListeners();
   }
 }
